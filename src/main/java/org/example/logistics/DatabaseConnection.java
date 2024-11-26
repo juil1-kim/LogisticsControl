f4abcb04cea2d91c9123e050c60216b159c570f7
Geunyeong Kim <gy940308@gmail.com>
2024-11-26 12:17:21 +0900

    All VO Files Upload

--- src/main/java/org/example/logistics/DatabaseConnection.java
+++ src/main/java/org/example/logistics/DatabaseConnection.java
@@ -4,31 +4,38 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// DAO 설계시
+// DatabaseConnection.getConnection()을 호출하여 Connection 객체를 가져오기만 하면 됩니다.
+// 향후 HikariCP 적용시 수정 필요합니다
+
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/mydb";
+    private static final String URL = "jdbc:mysql://localhost:3306/Logistics";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "1234";
 
     // Singleton Connection 관리 (Thread-Safe)
-    private static Connection connection = null;
+    private static Connection con = null;
 
     private DatabaseConnection() {}
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
-        if (connection == null || connection.isClosed()) {
+        // 연결이 없거나 닫혀 있으면 새로 생성
+        if (con == null || con.isClosed()) {
+
+            // 동기화 블록으로 Thread-Safe 구현
             synchronized (DatabaseConnection.class) {
-                if (connection == null || connection.isClosed()) {
-                    // 드라이버 로드
+                if (con == null || con.isClosed()) {
+                    // JDBC 1단계. 드라이버 설정
                     Class.forName("com.mysql.cj.jdbc.Driver");
                     System.out.println("Driver Loaded Successfully!");
 
-                    // 연결 생성
-                    connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                    // JDBC 2단계. DB 연결
+                    con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
                     System.out.println("Database Connected Successfully!");
                 }
             }
         }
-        return connection;
+        return con;
     }
 }
 
