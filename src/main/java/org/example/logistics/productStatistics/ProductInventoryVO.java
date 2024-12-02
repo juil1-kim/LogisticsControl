40777646e55875f8cbb7cf2d9edff7b9cb496f8a
Geunyeong Kim <98966788+Dubutoto@users.noreply.github.com>
2024-12-02 18:46:26 +0900

    develop 정상화

---
+++ src/main/java/org/example/logistics/productStatistics/ProductInventoryVO.java
@@ -0,0 +1,19 @@
+package org.example.logistics.productStatistics;
+
+import lombok.Data;
+
+@Data
+public class ProductInventoryVO {
+    private int productId;
+    private String productName;
+    private int quantity;
+    private String warehouseName;
+
+    // AllArgsConstructor를 통해 생성자도 Lombok으로 처리 가능
+    public ProductInventoryVO(int productId, String productName, int quantity, String warehouseName) {
+        this.productId = productId;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.warehouseName = warehouseName;
+    }
+}
---
+++ src/main/java/org/example/logistics/products/CategoriesVO.java
@@ -0,0 +1,11 @@
+package org.example.logistics.products;
+
+import lombok.Data;
+
+@Data
+public class CategoriesVO {
+    private int categoryId;
+    private String name;
+    private String description;
+}
+
